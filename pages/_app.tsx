84e5d9e6a0ef0d27ba78056e02b08d6326f4bc4d
Bbanez <banezprod@gmail.com>
2022-05-12 14:09:15 +0200

    Config bcms image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { BCMSImageConfig } from 'next-plugin-bcms/components';
+
+BCMSImageConfig.localeImageProcessing = true;
+BCMSImageConfig.cmsOrigin = process.env.NEXT_PUBLIC_BCMS_ORIGIN || '';
+BCMSImageConfig.publicApiKeyId =
+  process.env.NEXT_PUBLIC_BCMS_PUBLIC_KEY_ID || '';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
