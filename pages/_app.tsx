10d676615a381ee69d3e3bdb22f323a6ed66d2fb
Bbanez <banezprod@gmail.com>
2022-06-10 10:45:46 +0200

    Update bcms image config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { BCMSImageConfig } from 'next-plugin-bcms/components';
 
-BCMSImageConfig.localeImageProcessing = true;
-BCMSImageConfig.cmsOrigin = process.env.NEXT_PUBLIC_BCMS_ORIGIN || '';
+BCMSImageConfig.cmsOrigin =
+  process.env.NEXT_PUBLIC_BCMS_API_ORIGIN ||
+  'https://becomes-starter-projects.yourbcms.com';
 BCMSImageConfig.publicApiKeyId =
-  process.env.NEXT_PUBLIC_BCMS_PUBLIC_KEY_ID || '';
+  process.env.NEXT_PUBLIC_BCMS_API_PUBLIC_KEY_ID || '629dcd4dbcf5017354af6fe8';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
